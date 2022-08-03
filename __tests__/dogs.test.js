16b3ac24ca948178bb4a814f0831ac8958e40ff4
Chad Stabler <stablerpsn@gmail.com>
2022-08-03 16:32:47 -0500

    working get route for all dogs

---
+++ __tests__/dogs.test.js
@@ -0,0 +1,19 @@
+const pool = require('../lib/utils/pool');
+const setup = require('../data/setup');
+const request = require('supertest');
+const app = require('../lib/app');
+
+describe('backend-express-template routes', () => {
+  beforeEach(() => {
+    return setup(pool);
+  });
+  it('get /dogs should return all dogs', async () => {
+    const resp = await request(app).get('/dogs');
+
+    expect(resp.status).toBe(200);
+    expect(resp.body.length).toEqual(4);
+  });
+  afterAll(() => {
+    pool.end();
+  });
+});
--- sql/setup.sql
+++ sql/setup.sql
@@ -1,2 +1,18 @@
 -- Use this file to define your SQL tables
 -- The SQL in this file will be executed when you run `npm run setup-db`
+
+drop table if exists dogs;
+
+
+create table dogs (
+    id bigint generated always as identity primary key,
+    name varchar not null,
+    age int not null,
+    is_cool boolean not null
+);
+
+insert into dogs (name, age, is_cool) values
+('Sully', 4, true),
+('Homer', 12, true),
+('Francis', 5, true),
+('Phoebe', 2, true);
