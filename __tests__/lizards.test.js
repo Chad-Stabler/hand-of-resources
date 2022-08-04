30229c7b62589e44a12df9112ee7be1d33819388
Chad Stabler <stablerpsn@gmail.com>
2022-08-03 19:40:46 -0500

    working route for get /lizards/id

--- __tests__/lizards.test.js
+++ __tests__/lizards.test.js
@@ -13,6 +13,11 @@ describe('backend-express-template routes', () => {
     expect(resp.status).toBe(200);
     expect(resp.body.length).toEqual(4);
   });
+  it('get lizards/id should return a single lizard datapoint', async () => {
+    const resp = await request(app).get('/lizards/1');
+
+    expect(resp.body).toHaveProperty('name', 'Bearded Dragon');
+  });
   afterAll(() => {
     pool.end();
   });
