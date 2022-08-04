bbf323f8663481339abda5bb171f7871d3e1cd30
Chad Stabler <stablerpsn@gmail.com>
2022-08-03 20:37:44 -0500

    post/fishes route running properly

--- __tests__/fishes.test.js
+++ __tests__/fishes.test.js
@@ -18,6 +18,19 @@ describe('backend-express-template routes', () => {
 
     expect(resp.body).toHaveProperty('name', 'Largemouth Bass');
   });
+  it('post /fishes should insert a new fish into the database', async () => {
+    const newFish = {
+      name: 'Sheepshead',
+      size: '3 feet',
+      catch_difficulty: 'Medium'
+    };
+    const resp = await request(app).post('/fishes').send(newFish);
+    expect(resp.status).toBe(200);
+    expect(resp.body).toEqual({
+      id: expect.any(String),
+      ...newFish
+    });
+  });
   afterAll(() => {
     pool.end();
   });
